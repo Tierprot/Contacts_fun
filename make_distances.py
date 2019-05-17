49c058ff20bdb58d5a4f9dae3a1318491b4ac356
Mary <shokova-masha@mail.ru>
2019-05-17 10:56:55 +0300

    did it

--- make_distances.py
+++ make_distances.py
@@ -1,2 +1,7 @@
-def make_distances():
-    return
+def make_distances(list):
+    res = []
+    for el1 in list:
+        for el2 in list:
+            dist = ((((el1[1])[0]-(el2[1])[0])**2+((el1[1])[1]-(el2[1])[1])**2+((el1[1])[2]-(el2[1])[2])**2)**0.5)
+            res.append((el1[0], el2[0], dist))
+    return res
