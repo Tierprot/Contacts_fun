2e9aca57fd13c3ebda5cc8097ae65954c1375bbc
Artemiy-Saharov <artemiy.sakharov.2001@mail.ru>
2019-05-17 12:25:08 +0300

    Graph builder added

--- build_graph.py
+++ build_graph.py
@@ -1,2 +1,15 @@
-def build_graph():
-    return
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def build_plot(keks):
+    x = []
+    y = []
+    c = []
+    for i in keks:
+        x.append(i[0])
+        y.append(i[1])
+        c.append(i[2])
+    plt.hexbin(x, y, c)
+    plt.show()
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from make_coordinates import make_coordinates
 from make_distances import make_distances
-
+from build_graph import build_plot
 
 def name():
     pdb_id=input('enter pdb code\n')
@@ -11,3 +11,4 @@ if __name__ == '__main__':
    pdb_id, chain = name()
    coordinates = make_coordinates(pdb_id, chain)
    distances = make_distances(coordinates)
+   build_plot(distances)
