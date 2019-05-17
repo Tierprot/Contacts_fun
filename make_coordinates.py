fff116f4151d4749af1eab4b5637c7fdec461c60
Tuarie Pelle <orchestrahomosapien@gmail.com>
2019-05-17 11:24:22 +0300

    this function unpacks the pdb file

--- make_coordinates.py
+++ make_coordinates.py
@@ -1,2 +1,20 @@
-def make_coords():
-    return
+def make_coordinates(file, chain):
+    coords = []
+    with open(file, 'r') as pdb:
+        for line in pdb:
+            if 'ATOM' in line and 'CA' in line and line.split()[4]==chain:
+                line = line.split()
+                for n in line:
+                    n = int(line[5])
+                for numbers in line:
+                    numbers = []
+                for x in line:
+                    x = float(line[6])
+                for y in line:
+                    y = float(line[7])
+                for z in line:
+                    z = float(line[8])
+                    numbers = [x, y, z]
+                combo = n, numbers
+                coords.append(combo)
+    return coords
